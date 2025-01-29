335d5a8632c79d9365c7537129313fda80dcfa21
JonathanTubac <javiertubac1290.e@gmail.com>
2025-01-29 11:14:28 -0600

    datos y test

---
+++ src/test/java/uvg/edu/TestStack.java
@@ -0,0 +1,13 @@
+package uvg.edu;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestStack {
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
